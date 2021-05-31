4458452980ce0c085338fbf26e8544233f4f72f4
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-05-31 12:03:15 +0100

    Add files via upload

---
+++ May_21/Module1_Week1/javascript_intro.js
@@ -0,0 +1,16 @@
+console.log('Hello World!'); 
+let firstName = 'Miguel';
+console.log(firstName);
+let lastName = 'Braga'
+console.log(lastName);
+let age = 25;
+console.log(age);
+let studentName;
+studentName = 'Danilo';
+console.log(studentName);
+studentName = 'Helder';
+console.log(studentName);
+let email = 'mmdanilo@hotmail.com'
+let address = 'largo de sao rafael'
+console.log(email)
+console.log(address)
